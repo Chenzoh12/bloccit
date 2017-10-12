acb84c9f206ee1fe31de042390c40c61a0e8c794
Vincent Scotti <scottivincentj@gmail.com>
2017-10-12 21:56:53 +0000

    End of assignment 16. Submitting screen shot.

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,8 @@ module Workspace
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
+    # Do not swallow errors in after_commit/after_rollback callbacks
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths << File.join(config.root, "lib")
   end
-end
+end 
